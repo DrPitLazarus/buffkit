4ef33582afa881003644110dbd95926406186f1f
trgk <trgk@protonmail.ch>
2021-07-31 11:36:40 +0200

    Make the TBC init a tiny bit less awful

--- BuffKit/LobbyTimer/Patchers/UIMatchLobby_Awake.cs
+++ BuffKit/LobbyTimer/Patchers/UIMatchLobby_Awake.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 namespace BuffKit.LobbyTimer.Patchers
 {
@@ -14,98 +12,32 @@ namespace BuffKit.LobbyTimer.Patchers
         {
             var lobby = UIMatchLobby.Instance;
 
-            var textPrototypeButton = 
-                Object.Instantiate(lobby.shipCustomizationButton, lobby.shipCustomizationButton.transform.parent);
-            textPrototypeButton.transform.name = "prototypeButton";
-            textPrototypeButton.gameObject.SetActive(false);
-            
-            var spacerGo = new GameObject("Spacer");
-            spacerGo.transform.parent = textPrototypeButton.transform.parent;
-            var spacerLe = spacerGo.AddComponent<LayoutElement>();
-            spacerLe.flexibleWidth = 1f;
-
-            var tbcGo = new GameObject("Timer Button Container");
-            tbcGo.SetActive(false);
-            tbcGo.transform.parent = textPrototypeButton.transform.parent;
-
-            var tbc = tbcGo.AddComponent<TimerButtonContainer>();
-            TimerButtonContainer.Instance = tbc;
-            tbc.LayoutGroup = tbcGo.AddComponent<HorizontalLayoutGroup>();
-            tbc.LayoutElement = tbcGo.AddComponent<LayoutElement>();
-
-            tbc.LayoutGroup.spacing = 5f;
-            tbc.LayoutGroup.childForceExpandHeight = false;
-            tbc.LayoutGroup.childForceExpandWidth = false;
-            
-            var imagePrototypeButton = 
-                Object.Instantiate(lobby.engineerButton, lobby.shipCustomizationButton.transform.parent);
+            var imagePrototypeButton =
+                Object.Instantiate(lobby.engineerButton, lobby.engineerButton.transform.parent);
             imagePrototypeButton.transform.name = "Image Prototype Button";
             imagePrototypeButton.gameObject.SetActive(false);
 
-            var icons = new Dictionary<string, Texture2D>();
-            var assetPath = @"BepInEx\plugins\BuffKit\Assets\Timer";
-            var gp = Directory.GetCurrentDirectory();
-            var path = Path.Combine(gp, assetPath);
-            var files = Directory.GetFiles(path);
-            foreach (var file in files)
-            {
-                var data = File.ReadAllBytes(file);
-                var texture = new Texture2D(16, 16);
-                texture.LoadImage(data);
-                icons.Add(Path.GetFileNameWithoutExtension(file), texture);
-            }
+            //An empty layout element that takes up all the extra space available
+            //Pushes the timer container to the right
+            var spacerGo = new GameObject("Spacer");
+            spacerGo.transform.parent = imagePrototypeButton.transform.parent;
+            spacerGo.AddComponent<LayoutElement>().flexibleWidth = 1f;
 
-            GameObject iconGo;
-            Image image;
-            UIHoverTooltipTarget tt;
-            
-            tbc.StartTimerButton = 
-                Object.Instantiate(imagePrototypeButton, tbcGo.transform);
-            tbc.StartTimerButton.name = "Start Timer Button";
+            var tbcGo = new GameObject("Timer Button Container");
+            tbcGo.transform.parent = imagePrototypeButton.transform.parent;
+            tbcGo.SetActive(false);
 
-            tt = tbc.StartTimerButton.gameObject.GetComponent<UIHoverTooltipTarget>();
-            tt.tooltip = "Start the timer or overtime (or resume if currently paused)";
+            // But hey, at least I don't have to touch the cache, right?
+            var font = imagePrototypeButton
+                .transform
+                .parent
+                .FindChild("Ship Loadout Button/Label")
+                .gameObject
+                .GetComponent<Text>()
+                .font;
             
-            iconGo = tbc.StartTimerButton.transform.Find("Icon").gameObject;
-            image = iconGo.GetComponent<Image>();
-            image.sprite = Sprite.Create(icons["start"], new Rect(new Vector2(0, 0), new Vector2(128, 128)),
-                new Vector2(64, 64));
-            
-            tbc.PauseTimerButton = 
-                Object.Instantiate(imagePrototypeButton, tbcGo.transform);
-            tbc.PauseTimerButton.name = "Pause Timer Button";
-            
-            tt = tbc.PauseTimerButton.gameObject.GetComponent<UIHoverTooltipTarget>();
-            tt.tooltip = "Pause the timer, uses one of the 2 available 2 minute long pauses";
-            
-            iconGo = tbc.PauseTimerButton.transform.Find("Icon").gameObject;
-            image = iconGo.GetComponent<Image>();
-            image.sprite = Sprite.Create(icons["pause"], new Rect(new Vector2(0, 0), new Vector2(128, 128)),
-                new Vector2(64, 64));
-            
-            tbc.ExtendPauseButton = 
-                Object.Instantiate(imagePrototypeButton, tbcGo.transform);
-            tbc.ExtendPauseButton.name = "Extend Pause Button";
-            
-            tt = tbc.ExtendPauseButton.gameObject.GetComponent<UIHoverTooltipTarget>();
-            tt.tooltip = "Extend the pause, uses one of the 2 available 2 minute long pauses";
-            
-            iconGo = tbc.ExtendPauseButton.transform.Find("Icon").gameObject;
-            image = iconGo.GetComponent<Image>();
-            image.sprite = Sprite.Create(icons["extend"], new Rect(new Vector2(0, 0), new Vector2(128, 128)),
-                new Vector2(64, 64));
-            
-            tbc.RefPauseTimerButton = 
-                Object.Instantiate(imagePrototypeButton, tbcGo.transform);
-            tbc.RefPauseTimerButton.name = "Ref Pause Button";
-            
-            tt = tbc.RefPauseTimerButton.gameObject.GetComponent<UIHoverTooltipTarget>();
-            tt.tooltip = "Stop the timer until manually resumed";
-            
-            iconGo = tbc.RefPauseTimerButton.transform.Find("Icon").gameObject;
-            image = iconGo.GetComponent<Image>();
-            image.sprite = Sprite.Create(icons["halt"], new Rect(new Vector2(0, 0), new Vector2(128, 128)),
-                new Vector2(64, 64));
+            TimerButtonContainer.Instance = tbcGo.AddComponent<TimerButtonContainer>();
+            TimerButtonContainer.Instance.Initialize(imagePrototypeButton, font);
         }
     }
 }
--- BuffKit/LobbyTimer/TimerButtonContainer.cs
+++ BuffKit/LobbyTimer/TimerButtonContainer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -27,6 +29,84 @@ namespace BuffKit.LobbyTimer
         public void Awake()
         {
             Instance = this;
+        }
+
+        public void Initialize(Button prototype, Font font)
+        {
+            LayoutGroup = gameObject.AddComponent<HorizontalLayoutGroup>();
+            LayoutElement = gameObject.AddComponent<LayoutElement>();
+
+            LayoutGroup.spacing = 5f;
+            LayoutGroup.childForceExpandHeight = false;
+            LayoutGroup.childForceExpandWidth = false;
+            
+            var statusGo = new GameObject("Status Label");
+            statusGo.transform.parent = gameObject.transform;
+            statusGo.SetActive(false);
+            
+            MuseLog.Info(statusGo.ToString());
+            var statusLe = statusGo.AddComponent<LayoutElement>();
+            statusLe.minHeight = 30;
+            statusLe.preferredHeight = 30;
+            
+            
+            StatusLabel = statusGo.AddComponent<Text>();
+            StatusLabel.alignment = TextAnchor.MiddleCenter;
+            StatusLabel.text = "Waiting for start";
+            StatusLabel.fontSize = 16;
+            StatusLabel.font = font;
+            
+            var countdownGo = Instantiate(statusGo, gameObject.transform);
+            countdownGo.name = "Countdown Label";
+            countdownGo.GetComponent<Text>().text = "";
+            
+            //TODO: figure out how to do this better
+            var icons = new Dictionary<string, Texture2D>();
+            var assetPath = @"BepInEx\plugins\BuffKit\Assets\Timer";
+            var gp = Directory.GetCurrentDirectory();
+            var path = Path.Combine(gp, assetPath);
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                var data = File.ReadAllBytes(file);
+                var texture = new Texture2D(16, 16);
+                texture.LoadImage(data);
+                icons.Add(Path.GetFileNameWithoutExtension(file), texture);
+            }
+            
+            StartButton = Instantiate(prototype, gameObject.transform);
+            StartButton.name = "Start Timer Button";
+            StartButton.gameObject.GetComponent<UIHoverTooltipTarget>()
+                .tooltip = "Start the timer (or resume if currently paused)";
+            StartButton.transform.FindChild("Icon").gameObject.GetComponent<Image>()
+                .sprite = CreateSprite(icons["start"]);
+            
+            PauseButton = Instantiate(prototype, gameObject.transform);
+            PauseButton.name = "Pause Timer Button";
+            PauseButton.gameObject.GetComponent<UIHoverTooltipTarget>()
+                .tooltip = "Pause the timer, uses one of the 2 available 2 minute long pauses";
+            PauseButton.transform.FindChild("Icon").gameObject.GetComponent<Image>()
+                .sprite = CreateSprite(icons["pause"]);
+            
+            ExtendButton = Instantiate(prototype, gameObject.transform);
+            ExtendButton.name = "Extend Timer Button";
+            ExtendButton.gameObject.GetComponent<UIHoverTooltipTarget>()
+                .tooltip = "Extend the pause, uses one of the 2 available 2 minute long pauses";
+            ExtendButton.transform.FindChild("Icon").gameObject.GetComponent<Image>()
+                .sprite = CreateSprite(icons["extend"]);
+            
+            RefPauseButton = Instantiate(prototype, gameObject.transform);
+            RefPauseButton.name = "Ref Pause Timer Button";
+            RefPauseButton.gameObject.GetComponent<UIHoverTooltipTarget>()
+                .tooltip = "Stop the timer until manually resumed";
+            RefPauseButton.transform.FindChild("Icon").gameObject.GetComponent<Image>()
+                .sprite = CreateSprite(icons["halt"]);
+        }
+
+        private static Sprite CreateSprite(Texture2D icon)
+        {
+            return Sprite.Create(icon, new Rect(new Vector2(0, 0), new Vector2(128, 128)),
+                new Vector2(64, 64));
         }
 
         public void OnEnable()
