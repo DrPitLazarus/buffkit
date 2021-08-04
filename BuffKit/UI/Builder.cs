7928d2cd8915b1686c2b6df1753edf852a2293b9
Alex <47084301+aswift8@users.noreply.github.com>
2021-08-04 17:19:06 +0100

    Added button builder, minor changes to other builders

--- BuffKit/UI/Builder.cs
+++ BuffKit/UI/Builder.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using TMPro;
 
 namespace BuffKit.UI
 {
     public static class Builder
     {
-        public static GameObject BuildScrollView(Transform parent, out GameObject obContent)
+        public static GameObject BuildVerticalScrollView(Transform parent, out GameObject obContent)
         {
             // Scrollbar
             var obHandle = new GameObject("Handle");
@@ -59,46 +60,52 @@ namespace BuffKit.UI
             rt.offsetMax = new Vector2(-20, -10);
             var mask = obViewport.AddComponent<Mask>();
             mask.showMaskGraphic = false;
-            obViewport.AddComponent<Image>();
+            //mask.showMaskGraphic = true;
+            img = obViewport.AddComponent<Image>();
+            //img.color = new Color(1, 0, 0, .5f);
 
             obContent = new GameObject("Content");
             obContent.transform.SetParent(obViewport.transform);
+            rt = obContent.AddComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchorMax = new Vector2(1, 1);
+            rt.offsetMin = new Vector2(0, 0);
+            rt.offsetMax = new Vector2(0, 0);
+            rt.pivot = new Vector2(0, 1);
             var vlg = obContent.AddComponent<VerticalLayoutGroup>();
             vlg.childAlignment = TextAnchor.LowerRight;
             vlg.childForceExpandWidth = true;
             vlg.childForceExpandHeight = false;
             vlg.spacing = 10;
-            vlg.padding = new RectOffset(10, 10, 10, 10);
+            //vlg.padding = new RectOffset(10, 10, 10, 10);
             var csf = obContent.AddComponent<ContentSizeFitter>();
-            csf.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            //csf.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
             csf.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-            rt = obContent.GetComponent<RectTransform>();
-            rt.anchorMin = new Vector2(0, 0);
-            rt.anchorMax = new Vector2(1, 1);
-            rt.pivot = new Vector2(0, 1);
 
             // Scroll View
-            var scrollView = new GameObject("Scroll View");
-            rt = scrollView.AddComponent<RectTransform>();
+            var obScrollView = new GameObject("Scroll View");
+            rt = obScrollView.AddComponent<RectTransform>();
             rt.anchorMin = new Vector2(0, 0);
             rt.anchorMax = new Vector2(1, 1);
             rt.offsetMin = new Vector2(0, 0);
             rt.offsetMax = new Vector2(0, 0);
-            obScrollbarVertical.transform.SetParent(scrollView.transform);
-            obViewport.transform.SetParent(scrollView.transform);
-            var scrollRect = scrollView.AddComponent<ScrollRect>();
+            obScrollbarVertical.transform.SetParent(obScrollView.transform);
+            obViewport.transform.SetParent(obScrollView.transform);
+            var scrollRect = obScrollView.AddComponent<ScrollRect>();
             scrollRect.viewport = obViewport.GetComponent<RectTransform>();
             scrollRect.verticalScrollbar = sb;
             scrollRect.content = obContent.GetComponent<RectTransform>();
             scrollRect.horizontal = false;
             scrollRect.scrollSensitivity = 20;
+            csf = obScrollView.AddComponent<ContentSizeFitter>();
+            csf.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
 
-            scrollView.transform.SetParent(parent, false);
+            obScrollView.transform.SetParent(parent, false);
 
-            return scrollView;
+            return obScrollView;
         }
 
-        public static GameObject BuildLabel(Transform parent, string text, int fontSize = 18)
+        public static GameObject BuildLabel(Transform parent, string text, TextAlignmentOptions alignment = TextAlignmentOptions.Left, int fontSize = 18)
         {
             var obLabel = new GameObject("Label");
             //var le = obLabel.AddComponent<LayoutElement>();
@@ -108,7 +115,8 @@ namespace BuffKit.UI
             cText.text = text;
             cText.font = Resources.Font;
             cText.fontSize = fontSize;
-            cText.alignment = TextAlignmentOptions.Left;
+            cText.alignment = alignment;
+            cText.enableWordWrapping = false;
             obLabel.transform.SetParent(parent, false);
             return obLabel;
         }
@@ -127,6 +135,73 @@ namespace BuffKit.UI
             outline.effectColor = new Color32(0xA8, 0x90, 0x79, 0xFF);
             obPanel.transform.SetParent(parent, false);
             return obPanel;
+        }
+
+        public static GameObject BuildButton(Transform parent, UnityAction callback, string text, TextAlignmentOptions alignment = TextAlignmentOptions.Center, int fontSize = 18)
+        {
+            var obButton = new GameObject("Button");
+
+            var obBackground = new GameObject("Background");
+            var imgBackground = obBackground.AddComponent<Image>();
+            imgBackground.sprite = Resources.BlankIcon;
+            var rt = imgBackground.rectTransform;
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchorMax = new Vector2(1, 1);
+            rt.offsetMin = new Vector2(0, 0);
+            rt.offsetMax = new Vector2(0, 0);
+            obBackground.transform.SetParent(obButton.transform);
+
+            var obLabel = BuildLabel(obButton.transform, text, alignment, fontSize);
+            rt = obLabel.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchorMax = new Vector2(1, 1);
+            rt.offsetMin = new Vector2(15, 0);
+            rt.offsetMax = new Vector2(0, 0);
+
+            var obOutline = new GameObject("Outline");
+            var imgOutline = obOutline.AddComponent<Image>();
+            imgOutline.sprite = Resources.ButtonOutline;
+            imgOutline.type = Image.Type.Sliced;
+            rt = imgOutline.rectTransform;
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchorMax = new Vector2(1, 1);
+            rt.offsetMin = new Vector2(0, 0);
+            rt.offsetMax = new Vector2(0, 0);
+            obOutline.transform.SetParent(obButton.transform);
+
+            var button = obButton.AddComponent<Button>();
+            button.onClick.AddListener(callback);
+            button.targetGraphic = imgBackground;
+            var animator = obButton.AddComponent<Animator>();
+            animator.runtimeAnimatorController = Resources.ButtonAnimatorController;
+            button.transition = Selectable.Transition.Animation;
+
+            obButton.transform.SetParent(parent, false);
+
+            return obButton;
+        }
+
+        public static void TestBuilder(Transform parent)
+        {
+            var log = BepInEx.Logging.Logger.CreateLogSource("builder");
+
+            var obPanel = BuildPanel(parent);
+            var obScrollView = BuildVerticalScrollView(obPanel.transform, out GameObject obContent);
+            var obLabel = BuildLabel(obContent.transform, "First label", TextAlignmentOptions.Right, 13);
+            for (int i = 0; i < 5; i++)
+            {
+                var v = i;
+                BuildLabel(obContent.transform, $"Label {i + 1}", TextAlignmentOptions.Left, i + 13);
+            }
+            var button1 = BuildButton(obContent.transform, delegate { log.LogInfo("My First Button callback"); }, "My First Button");
+            var le = button1.AddComponent<LayoutElement>();
+            le.minHeight = 35;
+            var button2 = BuildButton(obContent.transform, delegate { log.LogInfo("My Second Button callback"); }, "My Second Button", TextAlignmentOptions.Left);
+            le = button2.AddComponent<LayoutElement>();
+            le.minHeight = 35;
+            var button3 = BuildButton(obContent.transform, delegate { log.LogInfo("My Third Button callback"); }, "My Third Button", TextAlignmentOptions.Left, 13);
+            le = button3.AddComponent<LayoutElement>();
+            le.minHeight = 25;
         }
 
     }
--- BuffKit/UI/Resources.cs
+++ BuffKit/UI/Resources.cs
@@ -11,6 +11,7 @@ namespace BuffKit.UI
         public static Sprite ButtonOutline { get; private set; }
         public static Sprite Checkmark { get; private set; }
         public static Sprite BetaBanner { get; private set; }
+        public static RuntimeAnimatorController ButtonAnimatorController { get; private set; }
 
         public static void _Initialize()
         {
@@ -25,6 +26,10 @@ namespace BuffKit.UI
             if (Checkmark == null) log.LogWarning("Could not find Checkmark");
             BetaBanner = GameObject.Find("/Game UI/Menu Canvas/UI Prototype Banner/Banner Image")?.GetComponent<Image>()?.sprite;
             if (BetaBanner == null) log.LogWarning("Could not find BetaBanner");
+            ButtonAnimatorController = GameObject.Find("/Menu UI/Standard Canvas/Pages/UI Match List/Browse Match Panel/Match Information Content/Create Button")?.GetComponent<Animator>()?.runtimeAnimatorController;
+            if (BetaBanner == null) log.LogWarning("Could not find ButtonAnimatorController");
+
+            //UI.Builder.TestBuilder(GameObject.Find("/Menu UI/Standard Canvas/Main Menu/Main Screen Elements/Game logo")?.transform);
         }
     }
 }
