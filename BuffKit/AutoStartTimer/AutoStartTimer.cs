e3887613619d389e1a25c2f293b24bd32dfa820b
Alex <47084301+aswift8@users.noreply.github.com>
2021-08-23 14:10:56 +0100

    Initial feature content

---
+++ BuffKit/AutoStartTimer/AutoStartTimer.cs
@@ -0,0 +1,28 @@
+﻿namespace BuffKit.AutoStartTimer
+{
+    public static class AutoStartTimer
+    {
+        private static bool _isEnabled = false;
+        public static void SetEnabled(bool isEnabled) { _isEnabled = isEnabled; }
+        public static void TryStartTimer()
+        {
+            var log = BepInEx.Logging.Logger.CreateLogSource("autostarttimer");
+            if (!_isEnabled)
+            {
+                log.LogInfo("Did not start timer - setting not active");
+                return;
+            }
+            log.LogInfo("Attempting to start match timer");
+            // TODO: check if timer is already running (find timer display, see how it is updated) - maybe not necessary?
+            if (Util.Util.HasModPrivilege(MatchLobbyView.Instance))
+            {
+                MatchActions.StartCountdown(20 * 60);
+                log.LogInfo("Called StartCountdown");
+            }
+            else
+            {
+                log.LogInfo("Did not start countdown - invalid permissions in lobby");
+            }
+        }
+    }
+}
