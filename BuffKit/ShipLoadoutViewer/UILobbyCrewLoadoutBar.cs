DrPitLazarus/buffkit

commit 2fcf3634d86dda727758f23c5f5f0e753d68968c

Moved crew loadout viewer left of profile button, hide crew loadout viewer if AI

BuffKit/ShipLoadoutViewer/UILobbyCrewLoadoutBar.cs
@@ -84,6 +84,7 @@ namespace BuffKit.ShipLoadoutViewer
             btn.interactable = false;                   // Switch to disabled colour (and prevent highlighting)
 
             obBar.transform.SetParent(parent);
+            obBar.transform.SetSiblingIndex(obBar.transform.GetSiblingIndex() - 1);
             return obBar;
         }
 
@@ -178,6 +179,8 @@ namespace BuffKit.ShipLoadoutViewer
         {
             if (data.PlayerClass != -1)
             {
+                gameObject.SetActive(true);
+
                 int column = data.PlayerClass;
 
                 _spacer1.SetActive(showTools[0, column] && showTools[1, column]);
@@ -210,10 +213,11 @@ namespace BuffKit.ShipLoadoutViewer
             }
             else
             {
-                for (var i = 0; i < 3; i++)
-                    _loadoutBarObjects[i].SetActive(false);
-                _spacer1.SetActive(false);
-                _spacer2.SetActive(false);
+                gameObject.SetActive(false);
+                //for (var i = 0; i < 3; i++)
+                //    _loadoutBarObjects[i].SetActive(false);
+                //_spacer1.SetActive(false);
+                //_spacer2.SetActive(false);
             }
         }
 

commit 583f442a05b0e5dd7de791bd0dc9d2adf8e15b3a

Fixes a bug introduced in 2fcf3634 which broke the crew loadout viewer visibility option

BuffKit/ShipLoadoutViewer/UILobbyCrewLoadoutBar.cs
@@ -179,8 +179,6 @@ namespace BuffKit.ShipLoadoutViewer
         {
             if (data.PlayerClass != -1)
             {
-                gameObject.SetActive(true);
-
                 int column = data.PlayerClass;
 
                 _spacer1.SetActive(showTools[0, column] && showTools[1, column]);