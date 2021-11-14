7c254be4a0fc88bf4b7df50898c28e592164c246
trgk <trgk@protonmail.ch>
2021-11-14 01:58:46 +0100

    [Chaos] Fix the 2v2v2v2 CK UI

---
+++ BuffKit/KothUiFix/KothUiFix.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace BuffKit.KothUiFix
+{
+    public static class KothUiFix
+    {
+        public static void CreateFix()
+        {
+            var prototype1 =
+                GameObject.Find(
+                    "Game UI/Match UI/UI HUD Canvas/UI Mission Specific Displays/UI Mission-Specific Displays/UI Mission-Specific Displays/Skirmish Control Point UI/Container/Team 1 Score");
+            var prototype2 =
+                GameObject.Find(
+                    "Game UI/Match UI/UI HUD Canvas/UI Mission Specific Displays/UI Mission-Specific Displays/UI Mission-Specific Displays/Skirmish Control Point UI/Container/Team 2 Score");
+            var team3 = Object.Instantiate(prototype1, prototype1.transform.parent);
+            team3.transform.SetSiblingIndex(2);
+            team3.name = "Team 3 Score";
+            var team4 = Object.Instantiate(prototype2, prototype2.transform.parent);
+            team4.transform.SetSiblingIndex(3);
+            team4.name = "Team 4 Score";
+
+            var uiCkHud = prototype1.transform.parent.parent.GetComponent<UIControlPointHUD>();
+            var newHuds = new UISkirmishHUD.TeamHUD[4];
+            Array.Copy(uiCkHud.teamHuds, newHuds, 2);
+            newHuds[2] = new UISkirmishHUD.TeamHUD
+            {
+                root = team3,
+                background = team3.transform.GetChild(0).GetComponent<Image>(),
+                scoreLabel = team3.transform.GetChild(1).GetComponent<Text>(),
+                myTeamIndicator = team3.transform.GetChild(2).GetComponent<Image>(),
+            };
+            
+            newHuds[3] = new UISkirmishHUD.TeamHUD
+            {
+                root = team4,
+                background = team4.transform.GetChild(0).GetComponent<Image>(),
+                scoreLabel = team4.transform.GetChild(1).GetComponent<Text>(),
+                myTeamIndicator = team4.transform.GetChild(2).GetComponent<Image>(),
+            };
+
+            uiCkHud.teamHuds = newHuds;
+            uiCkHud.teamHuds[2].background.color = TeamColors.GetColor(2);
+            uiCkHud.teamHuds[3].background.color = TeamColors.GetColor(3);
+
+            Vector3 temp;
+            
+            temp = uiCkHud.teamHuds[0].root.transform.localPosition;
+            temp.y += 12.5f;
+            uiCkHud.teamHuds[0].root.transform.localPosition = temp;
+            
+            temp = uiCkHud.teamHuds[1].root.transform.localPosition;
+            temp.y += 12.5f;
+            uiCkHud.teamHuds[1].root.transform.localPosition = temp;
+            
+            temp = uiCkHud.teamHuds[2].root.transform.localPosition;
+            temp.y -= 12.5f;
+            uiCkHud.teamHuds[2].root.transform.localPosition = temp;
+            
+            temp = uiCkHud.teamHuds[3].root.transform.localPosition;
+            temp.y -= 12.5f;
+            uiCkHud.teamHuds[3].root.transform.localPosition = temp;
+
+            var uicp = GameObject.Find(
+                "Game UI/Match UI/UI HUD Canvas/UI Mission Specific Displays/UI Mission-Specific Displays/UI Mission-Specific Displays/Skirmish Control Point UI/Container/UI Control Point");
+            var pipsOne = new Transform[4]
+            {
+                uicp.transform.FindChild("Team Zero Ship Pip 1"),
+                uicp.transform.FindChild("Team Zero Ship Pip 2"),
+                uicp.transform.FindChild("Team Zero Ship Pip 3"),
+                uicp.transform.FindChild("Team Zero Ship Pip 4")
+            };
+            
+            var pipsTwo = new Transform[4]
+            {
+                uicp.transform.FindChild("Team One Ship Pip 1"),
+                uicp.transform.FindChild("Team One Ship Pip 2"),
+                uicp.transform.FindChild("Team One Ship Pip 3"),
+                uicp.transform.FindChild("Team One Ship Pip 4")
+            };
+
+            foreach (var pip in pipsOne)
+            {
+                var a = pip.localPosition;
+                a.y += 12.5f;
+                pip.localPosition = a;
+            }
+            
+            foreach (var pip in pipsTwo)
+            {
+                var a = pip.localPosition;
+                a.y += 12.5f;
+                pip.localPosition = a;
+            }
+        }
+    }
+}
---
+++ BuffKit/KothUiFix/Patcher.cs
@@ -0,0 +1,114 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BuffKit.KothUiFix
+{
+    [HarmonyPatch(typeof(UIManager), "Start")]
+    class UIManager_Start
+    {
+        static void Postfix()
+        {
+            KothUiFix.CreateFix();
+        }
+    }
+
+    [HarmonyPatch(typeof(UIControlPointHUD), "OnUpdate")]
+    class UIControlPointHUD_OnUpdate
+    {
+        static bool Prefix(UIControlPointHUD __instance)
+        {
+            if (__instance.isActive && __instance.controlPoint != null && __instance.controlPoint.MatchInProgress)
+            {
+                if (__instance.kingOfTheHill != null)
+                {
+                    for (int i = 0; i < __instance.teamHuds.Length; i++)
+                    {
+                        if (__instance.kingOfTheHill.resourcesGathered != null &&
+                            __instance.kingOfTheHill.resourcesGathered.Length > i &&
+                            __instance.kingOfTheHill.resourcesGathered[i] != null &&
+                            __instance.kingOfTheHill.resourcesGathered[i].resources != null &&
+                            __instance.kingOfTheHill.resourcesGathered[i].resources.Length == 1)
+                        {
+                            __instance.teamHuds[i].scoreLabel.text =
+                                ((int)__instance.kingOfTheHill.resourcesGathered[i].resources[0].amount).ToString();
+                        }
+                        else
+                        {
+                            __instance.teamHuds[i].scoreLabel.text = "0";
+                        }
+                    }
+                }
+                if (__instance.crazyKing != null)
+                {
+                    int timeToNextHill = __instance.crazyKing.TimeToNextHill;
+                    __instance.countdownTimerText.text =
+                        string.Format("{0}:{1:D2}", timeToNextHill / 60, timeToNextHill % 60);
+                    if (__instance.lastActiveObjectiveIndex != __instance.currentlyActiveObjectiveIndex)
+                    {
+                        __instance.lastActiveObjectiveIndex = __instance.currentlyActiveObjectiveIndex;
+                        string text = ((char)(__instance.currentlyActiveObjectiveIndex + 65)).ToString();
+                        if (__instance.centralControlPointText != null)
+                        {
+                            __instance.centralControlPointText.text = text;
+                        }
+                        if (UIMatchAlertDisplay.instance != null)
+                        {
+                            UIMatchAlertDisplay.instance.EnqueueAlert(
+                                string.Format("Control Point {0} is Active", text), 0f, 0, text,
+                                UIMatchStateSoundType.None);
+                        }
+                    }
+                }
+                else if (__instance.centralControlPointText != null)
+                {
+                    __instance.centralControlPointText.text = string.Empty;
+                }
+                __instance.RefreshCentralControlPointIconForFirstActivePoint();
+            }
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(UIControlPointHUD), "RefreshControlPoint")]
+    class UIControlPointHUD_RefreshControlPoint
+    {
+        static bool Prefix(UIControlPointDisplayIcon controlPointIcon,
+            bool isActive,
+            float controlProgress,
+            int controllingTeam,
+            int capturingTeam,
+            int capPointCount,
+            int extraControlPointCount)
+        {
+            if (isActive)
+            {
+                if (controllingTeam == 0 || controllingTeam == 1 || controllingTeam == 2 || controllingTeam == 3)
+                {
+                    if (controlProgress == 0f && capturingTeam == -1)
+                    {
+                        controlProgress = 1f;
+                    }
+                    controlPointIcon.Fill(controlProgress, TeamColors.GetColor(controllingTeam));
+                    controlPointIcon.RecolorCenter(TeamColors.GetColor(controllingTeam));
+                }
+                else if (capturingTeam == 0 || capturingTeam == 1 || capturingTeam == 2  || capturingTeam == 3)
+                {
+                    controlPointIcon.Fill(controlProgress, TeamColors.GetColor(capturingTeam));
+                    controlPointIcon.RecolorCenter(Color.clear);
+                }
+                else
+                {
+                    controlPointIcon.ClearFill();
+                    controlPointIcon.RecolorCenter(Color.clear);
+                }
+            }
+            else
+            {
+                controlPointIcon.ClearFill();
+                controlPointIcon.RecolorCenter(Color.clear);
+            }
+
+            return false;
+        }
+    }
+}
