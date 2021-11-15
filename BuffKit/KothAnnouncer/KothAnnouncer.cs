c5559e4cdd1b04836b3116171a414f77133eee14
trgk <trgk@protonmail.ch>
2021-11-15 20:57:35 +0100

    [Chaos] Add a basic KotH/CK announcer

---
+++ BuffKit/KothAnnouncer/KothAnnouncer.cs
@@ -0,0 +1,52 @@
+﻿namespace BuffKit.KothAnnouncer
+{
+    public class KothAnnouncer
+    {
+        public static KothAnnouncer Instance;
+
+        private CrazyKing _currentMatch;
+        private int _previousTeam = -1;
+        private bool _enabled = true;
+
+        public static void Initialize()
+        {
+            Instance = new KothAnnouncer();
+        }
+
+        public void SetEnabled(bool v)
+        {
+            _enabled = v;
+        }
+
+        public void OnMatchInitialize(CrazyKing match)
+        {
+            MuseLog.Info("KOTH Announcer Initialized");
+            _currentMatch = match;
+        }
+
+        public void OnMatchUpdate(CrazyKing match)
+        {
+            if (!_enabled) return;
+            
+            if (match != _currentMatch)
+            {
+                OnMatchInitialize(match);
+            }
+
+            for (int i = 0; i < match.objectives.Length; i++)
+            {
+                var objective = match.objectives[i];
+                if (objective.active)
+                {
+                    if (objective.controllingTeam != _previousTeam)
+                    {
+                        Util.ForceSendMessage(
+                            $"[REF] {Util.GetTeamName(objective.controllingTeam).ToUpper()} captured the point! {(int)match.resourcesGathered[objective.controllingTeam].resources[0].amount}/{(int)match.resourceGoals[0].amount}");
+                        _previousTeam = objective.controllingTeam;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ BuffKit/KothAnnouncer/Patcher.cs
@@ -0,0 +1,61 @@
+﻿using BuffKit.MatchRefTools;
+using BuffKit.UI;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BuffKit.KothAnnouncer
+{
+    static class Patcher
+    {
+        private static bool _enableCKAnnouncer = false;
+        
+        public static void Init()
+        {
+            KothAnnouncer.Initialize();
+
+            Settings.Settings.Instance.AddEntry("chaos", "Enable CK Announcer", delegate (bool v)
+            {
+                _enableCKAnnouncer = v;
+                KothAnnouncer.Instance.SetEnabled(_enableCKAnnouncer);
+            }, _enableCKAnnouncer);
+
+        }
+    }
+    
+    [HarmonyPatch(typeof(CrazyKing), "OnRemoteUpdate")]
+    class CrazyKing_OnRemoteUpdate
+    {
+        private static bool _firstPrepare = true;
+        private static void Prepare()
+        {
+            if (_firstPrepare)
+            {
+                Util.OnGameInitialize += Patcher.Init;
+                _firstPrepare = false;
+            }
+        }
+        private static void Postfix(CrazyKing __instance)
+        {
+            KothAnnouncer.Instance.OnMatchUpdate(__instance);
+        }
+    }
+    
+    
+    class CrazyKing_OnMatchInitialize
+    {
+        private static bool _firstPrepare = true;
+        private static void Prepare()
+        {
+            if (_firstPrepare)
+            {
+                Util.OnGameInitialize += Patcher.Init;
+                _firstPrepare = false;
+            }
+        }
+        private static void Postfix(CrazyKing __instance)
+        {
+            KothAnnouncer.Instance.OnMatchUpdate(__instance);
+        }
+    }
+}
