2ab97864e78a4c5b1328e0c71f2aad424b99a90a
trgk <trgk@protonmail.ch>
2021-11-25 03:28:49 +0100

    [Chaos] WIP: add milestones to KothAnnouncer

--- BuffKit/KothAnnouncer/KothAnnouncer.cs
+++ BuffKit/KothAnnouncer/KothAnnouncer.cs
@@ -1,4 +1,7 @@
-﻿namespace BuffKit.KothAnnouncer
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuffKit.KothAnnouncer
 {
     public class KothAnnouncer
     {
@@ -7,6 +10,7 @@
         private CrazyKing _currentMatch;
         private int _previousTeam = -1;
         private bool _enabled = true;
+        private Dictionary<int, List<int>> _milestones = new Dictionary<int, List<int>>();
 
         public static void Initialize()
         {
@@ -21,8 +25,18 @@
         public void OnMatchInitialize(CrazyKing match)
         {
             MuseLog.Info("KOTH Announcer Initialized");
+            
             _currentMatch = match;
             _previousTeam = -1;
+            _milestones.Clear();
+            
+            foreach (var team in _milestones)
+            {
+                for (int i = 1; i <= Math.Floor(_currentMatch.resourceGoals[0].amount / 100); i++)
+                {
+                    team.Value.Add(i * 100);
+                }
+            }
         }
 
         public void OnMatchUpdate(CrazyKing match)
@@ -34,19 +48,23 @@
                 OnMatchInitialize(match);
             }
 
-            for (int i = 0; i < match.objectives.Length; i++)
+            foreach (var objective in match.objectives)
             {
-                var objective = match.objectives[i];
-                if (objective.active)
-                {
-                    if (objective.controllingTeam != _previousTeam)
-                    {
-                        Util.ForceSendMessage(
-                            $"[REF] {Util.GetTeamName(objective.controllingTeam).ToUpper()} captured the point! {(int)match.resourcesGathered[objective.controllingTeam].resources[0].amount}/{(int)match.resourceGoals[0].amount}");
-                        _previousTeam = objective.controllingTeam;
-                    }
-                    return;
-                }
+                if (!objective.active || objective.controllingTeam == _previousTeam) continue;
+                
+                Util.ForceSendMessage(
+                    $"REF: {Util.GetTeamName(objective.controllingTeam).ToUpper()} captured the point! {(int)match.resourcesGathered[objective.controllingTeam].resources[0].amount}/{(int)match.resourceGoals[0].amount}");
+                _previousTeam = objective.controllingTeam;
+                return;
+            }
+
+            for (var i = 0; i < match.numberOfTeams; i++)
+            {
+                var amount = (int)match.resourcesGathered[i].resources[0].amount;
+                if (amount != _milestones[i][0]) continue;
+                
+                Util.ForceSendMessage($"REF: {Util.GetTeamName(i)} reached {_milestones[i][0].ToString()} points!");
+                _milestones[i].RemoveAt(0);
             }
         }
     }
