0c9b38e2cf61c915734e7f5f9e7afb61da47866c
aswift8 <alexswift@live.co.uk>
2022-04-17 14:09:26 +0100

    Added options menu entry.

--- BuffKit/AutoAcceptLoadout/Patcher.cs
+++ BuffKit/AutoAcceptLoadout/Patcher.cs
@@ -7,10 +7,23 @@ namespace BuffKit.AutoAcceptLoadout
     [HarmonyPatch(typeof(UINewAcceptLoadoutDialog), "Show")]
     class UINewAcceptLoadoutDialog_Show
     {
+        private static bool enableAutoAccept = false;
+
+        private static bool firstPrepare = true;
+        private static void Prepare()
+        {
+            if (firstPrepare)
+            {
+                Settings.Settings.Instance.AddEntry("misc", "auto accept loadouts", v => enableAutoAccept = v, enableAutoAccept);
+                firstPrepare = false;
+            }
+        }
         private static bool Prefix(
             UINewAcceptLoadoutDialog __instance, ref int ___captainId, ref AvatarClass ___clazz, ref IList<SkillConfig> ___skills,
             int captainId, string captainName, AvatarClass clazz, IList<SkillConfig> skills)
         {
+            if (!enableAutoAccept) return true;
+
             ___captainId = captainId;
             ___clazz = clazz;
             ___skills = skills;
