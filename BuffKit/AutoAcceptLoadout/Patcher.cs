0215dc50ebc4743fae8e067e21527f94cd135eba
aswift8 <INeedToRememberMyPassword>
2022-04-17 13:55:05 +0100

    Automatic accept incoming recommended loadout feature added.

---
+++ BuffKit/AutoAcceptLoadout/Patcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Muse.Goi2.Entity;
+
+namespace BuffKit.AutoAcceptLoadout
+{
+    [HarmonyPatch(typeof(UINewAcceptLoadoutDialog), "Show")]
+    class UINewAcceptLoadoutDialog_Show
+    {
+        private static bool Prefix(
+            UINewAcceptLoadoutDialog __instance, ref int ___captainId, ref AvatarClass ___clazz, ref IList<SkillConfig> ___skills,
+            int captainId, string captainName, AvatarClass clazz, IList<SkillConfig> skills)
+        {
+            ___captainId = captainId;
+            ___clazz = clazz;
+            ___skills = skills;
+            __instance.gameObject.SetActive(true);
+
+            var flags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+            typeof(UINewAcceptLoadoutDialog).GetMethod("Accept", flags).Invoke(__instance, null);
+
+            return false;
+        }
+    }
+}
