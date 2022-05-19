2e0f3704f94633e673dc74db803306f6d21b98c8
aswift8 <alexswift@live.co.uk>
2022-05-19 17:30:57 +0100

    Added in-game match UI toggle with F6.

---
+++ BuffKit/ToggleMatchUI/Patcher.cs
@@ -0,0 +1,112 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BuffKit.ToggleMatchUI
+{
+    // Create component
+    [HarmonyPatch(typeof(Mission), "Start")]
+    class Mission_Start
+    {
+        private static void Postfix()
+        {
+            MuseLog.Info("Mission start postfix");
+            if (!NetworkedPlayer.Local.IsSpectator)
+            {
+                MuseLog.Info("Adding ToggleUIController");
+                Mission.Instance.gameObject.AddComponent<ToggleUIController>();
+            }
+        }
+    }
+
+    // Destroy component
+    [HarmonyPatch(typeof(Mission), "OnDisable")]
+    class Mission_OnDisable
+    {
+        private static void Postfix()
+        {
+            MuseLog.Info("Destroying ToggleUIController");
+            if (ToggleUIController.Initialized)
+                Object.Destroy(ToggleUIController.Instance);
+        }
+    }
+
+    // Hide hitmarkers(?)
+    [HarmonyPatch(typeof(UIOverlayHitIndicator), "Refresh")]
+    class UIOverlayHitIndicator_Refresh
+    {
+        private static bool Prefix(UIOverlayHitIndicator __instance)
+        {
+            if (__instance.gameObject.activeSelf)
+            {
+                MuseLog.Info("UIOverlayHitIndicator.Refresh called!");
+                if (ToggleUIController.Initialized && ToggleUIController.Instance.ShowUI)
+                    return true;
+                __instance.gameObject.SetActive(false);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    // Hide hitmarkers
+    [HarmonyPatch(typeof(UIOverlayManager), "UpdateHitPoint")]
+    class UIOverlayManager_UpdateHitPoint
+    {
+        private static void Postfix(UITransform uiTransform, HitPoint hit)
+        {
+            if (!ToggleUIController.Initialized || ToggleUIController.Instance.ShowUI) return;
+            uiTransform.DeactivateIfActivated(0f);
+        }
+    }
+
+    // Hide Alliance spotted ship
+    [HarmonyPatch(typeof(UIOverlayManager), "UpdateHighlightSpotting")]
+    class UIOverlayManager_UpdateHighlightSpotting
+    {
+        private static bool Prefix(UITransform[] ___radarBlips, UIGenericHealthBar[] ___shipHealth)
+        {
+            if (!ToggleUIController.Initialized || ToggleUIController.Instance.ShowUI) return true;
+            foreach (var blip in ___radarBlips)
+                blip.Deactivate(0f);
+            foreach (var health in ___shipHealth)
+                health.Deactivate();
+            return false;
+        }
+    }
+    // Hide Skirmish spotted ship
+    [HarmonyPatch(typeof(UIOverlayManager), "UpdateBracketSpotting")]
+    class UIOverlayManager_UpdateBracketSpotting
+    {
+        private static bool Prefix(UITransform[] ___radarBlips, UIGenericHealthBar[] ___shipHealth)
+        {
+            if (!ToggleUIController.Initialized || ToggleUIController.Instance.ShowUI) return true;
+            foreach (var blip in ___radarBlips)
+                blip.Deactivate(0f);
+            foreach (var health in ___shipHealth)
+                health.Deactivate();
+            return false;
+        }
+    }
+    // Hide Alliance captain-marked ship
+    [HarmonyPatch(typeof(UIOverlayManager), "UpdateHealthBar")]
+    class UIOverlayManager_UpdateHealthBar
+    {
+        private static bool Prefix(Ship ship, UIGenericHealthBar shipHealth, Color highlightColor)
+        {
+            if (!ToggleUIController.Initialized || ToggleUIController.Instance.ShowUI) return true;
+            shipHealth.Deactivate();
+            return false;
+        }
+    }
+
+    // Prevents error spam when on gun
+    [HarmonyPatch(typeof(UIReticle), "SetPosition")]
+    class UIReticle_SetPosition
+    {
+        private static bool Prefix(Vector3 worldPoint)
+        {
+            if (!ToggleUIController.Initialized || ToggleUIController.Instance.ShowUI) return true;
+            return false;
+        }
+    }
+}
---
+++ BuffKit/ToggleMatchUI/ToggleUIController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace BuffKit.ToggleMatchUI
+{
+    public class ToggleUIController : MonoBehaviour
+    {
+        public static ToggleUIController Instance;
+
+        public static bool Initialized { get; private set; }
+
+        private static KeyBinding _kb;
+
+        public bool ShowUI { get; private set; }
+        private List<GameObject> _objectsToHide = new List<GameObject>();
+
+        private void Awake()
+        {
+            Instance = this;
+            _kb = new KeyBinding(KeyCode.F6);
+            // UI object tree
+            _objectsToHide.Add(UIDisplayManager.instance.gameObject.transform.parent.gameObject);
+            // Match chat panel
+            _objectsToHide.Add(GameObject.Find("Menu UI/Standard Canvas/Menu Header Footer/Match Chat Panel"));
+            // First-person equipment object
+            _objectsToHide.Add(GameObject.Find("GameCameraRig/CutScene/MainCamera/FoVOffset/PlayerEquipment"));
+            ShowUI = true;
+            Initialized = true;
+        }
+        private void OnDisable()
+        {
+            Initialized = false;
+        }
+        public void LateUpdate()
+        {
+            if (_kb.GetDown())
+            {
+                ShowUI = !ShowUI;
+                MuseLog.Info("Show UI change to: " + ShowUI);
+                foreach (var ob in _objectsToHide)
+                    ob.SetActive(ShowUI);
+                if (ShowUI)
+                    UIRepairComponentView.Activate();
+            }
+            // Some things get re-activated elsewhere (e.g. change states, close pause menu, etc.) so disable every update
+            if (!ShowUI)
+            {
+                UIRepairComponentView.Deactivate();
+                foreach (var ob in _objectsToHide)
+                    ob.SetActive(false);
+            }
+        }
+    }
+}
