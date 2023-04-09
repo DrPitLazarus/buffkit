faed04bc11d4c8b0ee51485d6f660a7a16a2f291
aswift8 <alexswift@live.co.uk>
2023-04-09 21:16:36 +0100

    Initial file setup, nothing here yet

---
+++ BuffKit/HullDisplay/HullDisplay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuffKit.HullDisplay
+{
+    class HullDisplay
+    {
+    }
+}
---
+++ BuffKit/HullDisplay/Patcher.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Muse.Goi2.Entity.Vo;
+using UnityEngine;
+
+namespace BuffKit.HullDisplay
+{
+    [HarmonyPatch(typeof(UIShipHealthIndicatorBar), "SetHealthBar")]
+    class UILoadingLobbyState_Exit
+    {
+        private static bool _firstPrepare = true;
+        private static void Prepare()
+        {
+            if (_firstPrepare)
+            {
+                Util.OnGameInitialize += delegate {  };
+
+                _firstPrepare = false;
+            }
+        }
+
+        private static void Postfix(UIShipHealthIndicatorBar __instance, float value)
+        {
+
+        }
+    }
+}
