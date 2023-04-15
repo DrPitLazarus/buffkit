a00b843dcf8e471d43d381e7a2a01b52e4678521
aswift8 <alexswift@live.co.uk>
2023-04-15 22:46:18 +0100

    Added logic for retrieving ship hull at each tick.

--- BuffKit/HullDisplay/HullDisplay.cs
+++ BuffKit/HullDisplay/HullDisplay.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using Muse.Goi2.Entity;
 
 namespace BuffKit.HullDisplay
 {
-    class HullDisplay
+    public static class HullDisplay
     {
+
+        private static int squidHull = 0;
+        private static int galleonHull = 0;
+
+        public static void Initialize()
+        {
+            // Squid ID 13
+            squidHull = GetHullFromShipId(13);
+            // Galleon ID 14
+            galleonHull = GetHullFromShipId(14);
+
+            System.Console.WriteLine("Squid hull : " + squidHull + ", Galleon hull : " + galleonHull);
+        }
+
+        private static int GetHullFromShipId(int shipId)
+        {
+            var model = CachedRepository.Instance.Get<ShipModel>(shipId);
+            var shipHull = (from part in model.Parts
+                            where part.Type == ShipStaticPartType.RIGGING
+                            select part).Sum((ShipStaticPartEntity part) => part.ArmorNum);
+            return shipHull;
+        }
     }
 }
--- BuffKit/HullDisplay/Patcher.cs
+++ BuffKit/HullDisplay/Patcher.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using Muse.Goi2.Entity.Vo;
-using UnityEngine;
 
 namespace BuffKit.HullDisplay
 {
@@ -12,7 +10,7 @@ namespace BuffKit.HullDisplay
         {
             if (_firstPrepare)
             {
-                Util.OnGameInitialize += delegate {  };
+                Util.OnGameInitialize += delegate { HullDisplay.Initialize(); };
 
                 _firstPrepare = false;
             }
@@ -20,7 +18,19 @@ namespace BuffKit.HullDisplay
 
         private static void Postfix(UIShipHealthIndicatorBar __instance, float value)
         {
-
+            // TODO: 
+            //  Create display label
+            //  Ensure display label is only visible when it should be (tie to UIShipHealthIndicatorBar visibility??)
+            //  Make display label show current ship hull in whatever unit
+            //  Make settings menu for hull display
+            //      Options:
+            //          No display (default)            display is disabled
+            //          Ship units                      units are based on the current ship, basically just numerical version of bar
+            //          Squids                          units are based on Squid hull (450 as of time of writing this)
+            //          Galleons                        units are based on Galleon hull (1750 as of time of writing this)
+            var ship = NetworkedPlayer.Local.CurrentShip;
+            var hull = ship.ActiveHull;
+            System.Console.WriteLine("Armor: " + hull.Health + ", Hull: " + hull.CoreHealth);
         }
     }
 }
