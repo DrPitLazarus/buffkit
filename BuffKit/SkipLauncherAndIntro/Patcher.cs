9a08071b512865698d2f3262d60607ad92cc3343
Pit <drpitlazarus@gmail.com>
2024-02-10 16:52:21 -0500

    BuffKit: Upgrade IntroSkip to SkipLauncherAndIntro, bump to 485.2.0 This keeps the launcher screen unlike mine.

---
+++ BuffKit/SkipLauncherAndIntro/Patcher.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace BuffKit.SkipLauncherAndIntro
+{
+    [HarmonyPatch(typeof(UIManager.UINewMainMenuState), "Update")]
+    class SkipLauncherAndIntro
+    {
+        private static bool enableSkip = true;
+
+        private static bool firstPrepare = true;
+        private static void Prepare()
+        {
+            if (firstPrepare)
+            {
+                Settings.Settings.Instance.AddEntry("misc", "skip launcher and intro", v => enableSkip = v, enableSkip);
+                firstPrepare = false;
+            }
+        }
+
+        private static void Postfix()
+        {
+            if (!enableSkip) return;
+            // Update is called when the game is connected, same function that enables the play button.
+            UILauncherMainPanel.ForceCloseWithoutCallback();
+        }
+    }
+}
