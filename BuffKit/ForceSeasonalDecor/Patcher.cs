abe5f06e2b8d0cc7a70e0e459abfc9ec4511845e
Pit <drpitlazarus@gmail.com>
2024-02-12 19:52:09 -0500

    BuffKit: Add undescore to private fields

--- BuffKit/ForceSeasonalDecor/Patcher.cs
+++ BuffKit/ForceSeasonalDecor/Patcher.cs
@@ -7,19 +7,19 @@ namespace BuffKit.ForceSeasonalDecor
     [HarmonyPatch(typeof(MuseWorldClient), "SetupCache")]
     public class ForceSeasonalDecor
     {
-        private static bool forceFireworks = false;
-        private static bool forceSpookyAi = false;
-        private static bool forceChristmasTrees = false;
-        private static bool firstPrepare = true;
+        private static bool _forceFireworks = false;
+        private static bool _forceSpookyAi = false;
+        private static bool _forceChristmasTrees = false;
+        private static bool _firstPrepare = true;
 
         private static void Prepare()
         {
-            if (firstPrepare)
+            if (_firstPrepare)
             {
-                Settings.Settings.Instance.AddEntry("misc", "force seasonal fireworks", v => forceFireworks = v, forceFireworks);
-                Settings.Settings.Instance.AddEntry("misc", "force seasonal spooky ai", v => forceSpookyAi = v, forceSpookyAi);
-                Settings.Settings.Instance.AddEntry("misc", "force seasonal christmas trees", v => forceChristmasTrees = v, forceChristmasTrees);
-                firstPrepare = false;
+                Settings.Settings.Instance.AddEntry("misc", "force seasonal fireworks", v => _forceFireworks = v, _forceFireworks);
+                Settings.Settings.Instance.AddEntry("misc", "force seasonal spooky ai", v => _forceSpookyAi = v, _forceSpookyAi);
+                Settings.Settings.Instance.AddEntry("misc", "force seasonal christmas trees", v => _forceChristmasTrees = v, _forceChristmasTrees);
+                _firstPrepare = false;
             }
         }
 
@@ -27,7 +27,7 @@ namespace BuffKit.ForceSeasonalDecor
         [HarmonyPostfix]
         private static void Fireworks(PlayerAirshipAssetBehaviour __instance, ref OneShotEffectSystem ___fireworksDestroyedEffect)
         {
-            if (!forceFireworks) return;
+            if (!_forceFireworks) return;
             MuseLog.Info("Enable fireworks!");
             if (___fireworksDestroyedEffect != null)
             {
@@ -39,7 +39,7 @@ namespace BuffKit.ForceSeasonalDecor
         [HarmonyPostfix]
         private static void SpookyAi(ref PlayerGraphics ___graphics, ref int ___userId)
         {
-            if (!forceSpookyAi) return;
+            if (!_forceSpookyAi) return;
             MuseLog.Info("Enable spooky AI!");
             ___graphics.IsGhost = ___userId <= 0;
         }
@@ -48,7 +48,7 @@ namespace BuffKit.ForceSeasonalDecor
         [HarmonyPostfix]
         private static void ChristmasTrees(ref GameObject ___christmasTree)
         {
-            if (!forceChristmasTrees) return;
+            if (!_forceChristmasTrees) return;
             MuseLog.Info("Enable christmas trees!");
             ___christmasTree.SetActive(true);
         }
--- BuffKit/SkipLauncherAndIntro/Patcher.cs
+++ BuffKit/SkipLauncherAndIntro/Patcher.cs
@@ -5,21 +5,21 @@ namespace BuffKit.SkipLauncherAndIntro
     [HarmonyPatch(typeof(UIManager.UINewMainMenuState), "Update")]
     class SkipLauncherAndIntro
     {
-        private static bool enableSkip = true;
+        private static bool _enableSkip = true;
+        private static bool _firstPrepare = true;
 
-        private static bool firstPrepare = true;
         private static void Prepare()
         {
-            if (firstPrepare)
+            if (_firstPrepare)
             {
-                Settings.Settings.Instance.AddEntry("misc", "skip launcher and intro", v => enableSkip = v, enableSkip);
-                firstPrepare = false;
+                Settings.Settings.Instance.AddEntry("misc", "skip launcher and intro", v => _enableSkip = v, _enableSkip);
+                _firstPrepare = false;
             }
         }
 
         private static void Postfix()
         {
-            if (!enableSkip) return;
+            if (!_enableSkip) return;
             // Update is called when the game is connected, same function that enables the play button.
             UILauncherMainPanel.ForceCloseWithoutCallback();
         }
