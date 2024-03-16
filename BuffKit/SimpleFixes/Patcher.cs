a2dbc2b8e6dc8eeebe049c1c588dab63b8b95151
Pit <drpitlazarus@gmail.com>
2024-03-15 23:24:57 -0400

    BuffKit: SmallFixes RemoveSpaceAfterBlueTeam

---
+++ BuffKit/SimpleFixes/Patcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace BuffKit.SimpleFixes
+{
+    [HarmonyPatch]
+    public class SimpleFixes
+    {
+        [HarmonyPatch(typeof(TeamColors), "GetName")]
+        [HarmonyPostfix]
+        private static void RemoveSpaceAfterBlueTeam(ref string __result)
+        {
+            __result = __result.Trim();
+        }
+    }
+}
