7acfd8db2d458cda1859ee5ffd7ce0c1418ca640
Pit <drpitlazarus@gmail.com>
2024-03-15 23:23:46 -0400

    BuffKit: SkirmishAlerts Deathmatch Done, Hopefully -Add alerts have sound setting -Add last ship death -Clean up some methods

--- BuffKit/SkirmishAlerts/Patcher.cs
+++ BuffKit/SkirmishAlerts/Patcher.cs
@@ -13,31 +13,39 @@ namespace BuffKit.SkirmishAlerts
     [HarmonyPatch]
     public class SkirmishAlerts
     {
-        private static readonly TimeSpan _announcementDelay = TimeSpan.FromSeconds(0.1);
+        private static readonly TimeSpan _alertDelay = TimeSpan.FromSeconds(0.1);
 
         private static bool _enabled = true;
         private static bool _logAlertsInChat = true;
+        private static bool _alertsHaveSound = true;
         private static bool _firstPrepare = true;
+
+        private static UIMatchStateSoundType _alertSound => _alertsHaveSound ? UIMatchStateSoundType.Normal : UIMatchStateSoundType.None;
 
         private static AnnouncementInfo _cachedAnnouncement;
         private static DateTime? _announcementTime;
-        private static Func<string> _announcementRawCallback;
-        private static Func<string> _announcementLogCallback;
+        private static Func<string> _alertRawCallback;
+        private static Func<string> _alertLogCallback;
+        private static string _lastShipDeath;
 
         private static void Prepare()
         {
             if (!_firstPrepare) return;
             Settings.Settings.Instance.AddEntry("skirmish alerts", "skirmish alerts", v => _enabled = v, _enabled);
             Settings.Settings.Instance.AddEntry("skirmish alerts", "log alerts in chat", v => _logAlertsInChat = v, _logAlertsInChat);
+            Settings.Settings.Instance.AddEntry("skirmish alerts", "alerts have sound", v => _alertsHaveSound = v, _alertsHaveSound);
             _firstPrepare = false;
         }
 
         [HarmonyPatch(typeof(Mission), "Start")]
         [HarmonyPostfix]
-        private static void EditAlertObject()
+        private static void Mission_Start()
         {
             if (!_enabled) return;
 
+            ResetAll();
+
+            // Edit Alert Object
             var textBoxObjectPath = "/Game UI/Match UI/UI HUD Canvas/UI Alert Display/Alert Root/Text Box/";
             // Increase height of the alert text box. Original 45.
             GameObject.Find(textBoxObjectPath).GetComponent<LayoutElement>().preferredHeight = 50;
@@ -55,17 +63,38 @@ namespace BuffKit.SkirmishAlerts
         [HarmonyPostfix]
         private static void Mission_Update()
         {
-            // Used to process delayed announcements.
+            // Used to process delayed alerts.
             if (!_enabled) return;
 
-            if (_announcementTime != null && DateTime.Now - _announcementTime >= _announcementDelay)
+            if (_announcementTime != null && DateTime.Now - _announcementTime >= _alertDelay)
             {
-                UIMatchAlertDisplay.instance.EnqueueRawAlert(_announcementRawCallback());
-                if (_logAlertsInChat) MuseWorldClient.Instance.ChatHandler.AddMessage(ChatMessage.Console(_announcementLogCallback()));
-                _announcementTime = null;
-                _announcementRawCallback = null;
-                _announcementLogCallback = null;
+                EnqueueRawAlert(_alertRawCallback());
+                if (_logAlertsInChat) LogMessageInChat(_alertLogCallback());
+                ResetAlert();
             }
+        }
+
+        [HarmonyPatch(typeof(Ship), "OnRemoteDestroy")]
+        [HarmonyPostfix]
+        private static void Ship_OnRemoteDestroy(Ship __instance)
+        {
+            if (!_enabled) return;
+
+            _lastShipDeath = __instance.GetDisplayName();
+        }
+
+        [HarmonyPatch(typeof(UIManager.UIMatchCompleteState), "EnterState")]
+        [HarmonyPostfix]
+        private static void UIManager_UIMatchCompleteState_EnterState()
+        {
+            if (!_enabled) return;
+
+            var deathmatch = (Deathmatch)Mission.Instance;
+            if (deathmatch == null) return;
+
+            var message = ScoresToString(deathmatch.Frags);
+            message += $". Last death was {_lastShipDeath}.";
+            if (_logAlertsInChat) LogMessageInChat(message);
         }
 
         [HarmonyPatch(typeof(UIAnnouncementDisplay), "HandleAnnouncement")]
@@ -73,32 +102,25 @@ namespace BuffKit.SkirmishAlerts
         private static void HandleAnnouncement(LinkedList<AnnouncementInfo> ___cachedAnnouncements)
         {
             // Called when an announcement is received from the server (top-left feed).
+            // Last kill is not announced from the server, sadly.
             if (!_enabled) return;
 
-            _cachedAnnouncement = ___cachedAnnouncements.First.Value;
+            var announcement = ___cachedAnnouncements.First.Value;
 
-            switch (_cachedAnnouncement.Announcement.Verb)
+            if (announcement.Announcement.Verb == AnnouncementVerb.Killed)
             {
-                case AnnouncementVerb.Killed:
-                    HandleKillAnnouncement();
-                    break;
-                default:
-                    break;
+                _cachedAnnouncement = announcement;
+                HandleKillAnnouncement();
             }
         }
 
         private static void HandleKillAnnouncement()
         {
-            // Last kill is not announced from the server, sadly.
-            var deathmatch = Mission.Instance as Deathmatch;
-            if (deathmatch == null)
-            {
-                MuseLog.Info("Deathmatch is null!");
-                return;
-            }
+            var deathmatch = (Deathmatch)Mission.Instance;
+            if (deathmatch == null) return;
 
-            var formattedAlertText = $";0;10;;1;{_cachedAnnouncement.formattedText}"; // Alert title is added before in raw callback.
-            var alertText = "{0} {1} {2}".F(
+            var formattedSubtitleText = _cachedAnnouncement.formattedText;
+            var alertSubtitleText = "{0} {1} {2}".F(
             [
                 UIAnnouncementDisplay.instance.GetSubjectText(_cachedAnnouncement.Announcement),
                 UIAnnouncementDisplay.instance.GetVerbText(_cachedAnnouncement.Announcement),
@@ -109,34 +131,69 @@ namespace BuffKit.SkirmishAlerts
             var withText = _cachedAnnouncement.Announcement.With?.Name ?? "";
             if (withText.Length > 0)
             {
-                formattedAlertText += $"\nwith {withText}";
-                alertText += $" with {withText}";
+                formattedSubtitleText += $"\nwith {withText}";
+                alertSubtitleText += $" with {withText}";
             }
 
             // Frags is not always updated when HandleAnnouncement is called. Need to call it a few frames later.
-            _announcementRawCallback = () =>
+            _alertRawCallback = () =>
             {
-                var mainText = "";
-                var scoreIndex = 0;
-
-                foreach (var score in deathmatch.Frags)
-                {
-                    var color = TeamColors.GetColorAsHex(scoreIndex) + "FF";
-                    mainText += $"<color=#{color}>{score}</color> - ";
-                    scoreIndex++;
-                }
-
-                mainText = mainText.Remove(mainText.Length - 3); // Remove last " - ".
-                return mainText + formattedAlertText;
+                var mainText = ScoresToString(deathmatch.Frags, formatTeamColors: true);
+                return BuildRawAlert(mainText, formattedSubtitleText, icon: 10, sound: _alertSound); // 10 is Boss icon.
             };
 
-            _announcementLogCallback = () =>
+            _alertLogCallback = () =>
             {
-                var mainText = string.Join(" - ", Array.ConvertAll(deathmatch.Frags, v => v.ToString()));
-                return $"{mainText}. {alertText}.";
+                var mainText = ScoresToString(deathmatch.Frags);
+                return $"{mainText}. {alertSubtitleText}.";
             };
 
             _announcementTime = DateTime.Now;
+        }
+
+        private static string BuildRawAlert(string title, string subtitle = "", int icon = -1, UIMatchStateSoundType sound = UIMatchStateSoundType.Normal, int duration = 0, string textInIcon = "")
+        {
+            // Icon required for subtitle to show.
+            return $"{title.Trim()};{duration};{icon};{textInIcon.Trim()};{(int)sound};{subtitle.Trim()}";
+        }
+
+        private static void EnqueueRawAlert(string rawAlert)
+        {
+            UIMatchAlertDisplay.instance.EnqueueRawAlert(rawAlert);
+        }
+
+        private static void LogMessageInChat(string message)
+        {
+            MuseWorldClient.Instance.ChatHandler.AddMessage(ChatMessage.Console(message));
+        }
+
+        private static void ResetAlert()
+        {
+            _cachedAnnouncement = null;
+            _announcementTime = null;
+            _alertRawCallback = null;
+            _alertLogCallback = null;
+        }
+
+        private static void ResetAll()
+        {
+            ResetAlert();
+            _lastShipDeath = null;
+        }
+
+        private static string ScoresToString(int[] scoresArray, bool formatTeamColors = false)
+        {
+            if (!formatTeamColors) return string.Join(" - ", Array.ConvertAll(scoresArray, v => v.ToString()));
+
+            var text = "";
+            var scoreIndex = 0;
+            foreach (var score in scoresArray)
+            {
+                var color = TeamColors.GetColorAsHex(scoreIndex) + "FF";
+                text += $"<color=#{color}>{score}</color> - ";
+                scoreIndex++;
+            }
+            return text.Remove(text.Length - 3); // Remove last " - ".
         }
     }
 }
