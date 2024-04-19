459ba4191425b3b2930971685ee7794a4e5dbdc0
Pit <drpitlazarus@gmail.com>
2024-04-18 23:49:44 -0400

    BuffKit: RepairCluster Prototype Add Indicator Struct and Colors

--- BuffKit/RepairCluster/Patcher.cs
+++ BuffKit/RepairCluster/Patcher.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,9 +12,66 @@ namespace BuffKit.RepairCluster
         private static readonly int _numberOfEngines = 4;
         private static readonly int _gridHeight = 42;
         private static readonly int _gridWidth = 32;
+        private static readonly Color _colorRed = new(1, 0, 0, 1);
+        private static readonly Color _colorRedTransparent = new(1, 0, 0, 0.6f);
+        private static readonly Color _colorWhite = new(1, 1, 1, 0.8f);
+        private static readonly Color _colorWhiteTransparent = new(1, 1, 1, 0.5f);
         private static readonly RectOffset _progressBarPadding = new(2, 2, 0, 0);
+        private static readonly List<Indicator> _indicators = [];
 
         private static bool _firstMainMenuState = true;
+
+        private struct Indicator
+        {
+            public string Name;
+            public Texture2D ComponentTexture;
+            public RawImage IconRawImage;
+            public LayoutElement ProgressBarLayoutElement;
+
+            public readonly void SetHealth(float percentage)
+            {
+                ProgressBarLayoutElement.preferredWidth = percentage * (_gridWidth - _progressBarPadding.horizontal);
+            }
+
+            public readonly void SetIconNormal()
+            {
+                IconRawImage.texture = ComponentTexture;
+            }
+
+            public readonly void SetIconFire()
+            {
+                IconRawImage.texture = UIRepairComponentView.instance.fireNormal;
+            }
+
+            public readonly void SetIconFireBig()
+            {
+                IconRawImage.texture = UIRepairComponentView.instance.fire;
+            }
+            public readonly void SetIconColorNormal()
+            {
+                IconRawImage.color = _colorWhite;
+            }
+
+            public readonly void SetIconColorNormalFaded()
+            {
+                IconRawImage.color = _colorWhiteTransparent;
+            }
+
+            public readonly void SetIconColorDamaged()
+            {
+                IconRawImage.color = _colorRedTransparent;
+            }
+
+            public readonly void SetIconColorDestroyed()
+            {
+                IconRawImage.color = _colorRed;
+            }
+
+            public override readonly string ToString()
+            {
+                return $"{Name}";
+            }
+        }
 
         [HarmonyPatch(typeof(UIManager.UINewMainMenuState), nameof(UIManager.UINewMainMenuState.Enter))]
         [HarmonyPostfix]
@@ -24,7 +82,7 @@ namespace BuffKit.RepairCluster
             CreateUi();
         }
 
-        private static void CreateUi()
+        private static GameObject CreateUi()
         {
             var parentObject = GameObject.Find("/Game UI/Match UI/UI HUD Canvas/UI HUD/UI Ship Health Display/Pilot Skill and Buff");
 
@@ -47,9 +105,11 @@ namespace BuffKit.RepairCluster
             {
                 CreateIndicator($"Engine {index + 1}", UIRepairComponentView.instance.enginesBig, mainObject.transform);
             }
+
+            return mainObject;
         }
 
-        private static void CreateIndicator(string name, Texture2D componentTexture, Transform parentTransform)
+        private static GameObject CreateIndicator(string name, Texture2D componentTexture, Transform parentTransform)
         {
             LayoutElement le;
             VerticalLayoutGroup vlg;
@@ -63,10 +123,11 @@ namespace BuffKit.RepairCluster
             var mainIconObject = new GameObject("Main Icon");
             mainIconObject.transform.SetParent(indicatorObject.transform, false);
             le = mainIconObject.AddComponent<LayoutElement>();
-            le.preferredHeight = _gridWidth;
-            le.preferredWidth = _gridWidth; // Intentional to make image 1:1.
+            le.preferredHeight = _gridWidth; // Intentional to make image 1:1.
+            le.preferredWidth = _gridWidth;
             var rawImage = mainIconObject.AddComponent<RawImage>();
             rawImage.texture = componentTexture;
+            rawImage.color = _colorWhite;
 
             var progressBarsObject = new GameObject("Progress Bars");
             progressBarsObject.transform.SetParent(indicatorObject.transform, false);
@@ -78,24 +139,29 @@ namespace BuffKit.RepairCluster
             vlg.childForceExpandWidth = false;
             vlg.padding = _progressBarPadding;
 
-            CreateProgressBar("Health", 4, ProgressBarPercentToWidth(1f), progressBarsObject.transform);
-            //CreateProgressBar("Buff", 3, ProgressBarPercentToWidth(.5f), progressBarsObject.transform);
-            //CreateProgressBar("Cooldown", 3, ProgressBarPercentToWidth(.25f), progressBarsObject.transform);
+            CreateProgressBar("Health", 4, progressBarsObject.transform, out LayoutElement progressBarLe);
+
+            var indicator = new Indicator
+            {
+                Name = name,
+                ComponentTexture = componentTexture,
+                IconRawImage = rawImage,
+                ProgressBarLayoutElement = progressBarLe,
+            };
+            _indicators.Add(indicator);
+            return indicatorObject;
         }
 
-        private static void CreateProgressBar(string name, int height, float width, Transform parentTransform)
+        private static GameObject CreateProgressBar(string name, int height, Transform parentTransform, out LayoutElement layoutElement)
         {
             var progressBarObject = new GameObject($"Progress Bar {name}");
             progressBarObject.transform.SetParent(parentTransform, false);
-            var le = progressBarObject.AddComponent<LayoutElement>();
-            le.preferredHeight = height;
-            le.preferredWidth = width;
-            progressBarObject.AddComponent<RawImage>();
-        }
-
-        private static float ProgressBarPercentToWidth(float percentage)
-        {
-            return percentage * (_gridWidth - _progressBarPadding.horizontal);
+            layoutElement = progressBarObject.AddComponent<LayoutElement>();
+            layoutElement.preferredHeight = height;
+            layoutElement.preferredWidth = _gridWidth - _progressBarPadding.horizontal;
+            var rawImage = progressBarObject.AddComponent<RawImage>();
+            rawImage.color = _colorWhite;
+            return progressBarObject;
         }
     }
 }
