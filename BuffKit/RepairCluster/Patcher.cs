d64a5871acc15b6a19607cb40ea9244c6c8ebb2f
Pit <drpitlazarus@gmail.com>
2024-04-20 23:59:11 -0400

    BuffKit: RepairCluster Prototype Add Basic Update Logic -Changed colors -Updates health bar and icon color

--- BuffKit/RepairCluster/Patcher.cs
+++ BuffKit/RepairCluster/Patcher.cs
@@ -6,72 +6,24 @@ using UnityEngine.UI;
 namespace BuffKit.RepairCluster
 {
     [HarmonyPatch]
-    public class RepairCluster
+    public class RepairCluster : MonoBehaviour
     {
         private static readonly int _numberOfGuns = 6;
         private static readonly int _numberOfEngines = 4;
         private static readonly int _gridHeight = 42;
         private static readonly int _gridWidth = 32;
-        private static readonly Color _colorRed = new(1, 0, 0, 1);
-        private static readonly Color _colorRedTransparent = new(1, 0, 0, 0.6f);
-        private static readonly Color _colorWhite = new(1, 1, 1, 0.8f);
-        private static readonly Color _colorWhiteTransparent = new(1, 1, 1, 0.5f);
+        private static readonly Color _colorRedDestroyed = new(1, 0, 0, 1);
+        private static readonly Color _colorOrangeDamaged = new(1, 0.5f, 0, 0.7f);
+        private static readonly Color _colorWhite = new(1, 1, 1, 0.5f);
+        private static readonly Color _colorWhiteTransparent = new(1, 1, 1, 0.3f);
         private static readonly RectOffset _progressBarPadding = new(2, 2, 0, 0);
         private static readonly List<Indicator> _indicators = [];
 
         private static bool _firstMainMenuState = true;
-
-        private struct Indicator
-        {
-            public string Name;
-            public Texture2D ComponentTexture;
-            public RawImage IconRawImage;
-            public LayoutElement ProgressBarLayoutElement;
-
-            public readonly void SetHealth(float percentage)
-            {
-                ProgressBarLayoutElement.preferredWidth = percentage * (_gridWidth - _progressBarPadding.horizontal);
-            }
-
-            public readonly void SetIconNormal()
-            {
-                IconRawImage.texture = ComponentTexture;
-            }
-
-            public readonly void SetIconFire()
-            {
-                IconRawImage.texture = UIRepairComponentView.instance.fireNormal;
-            }
-
-            public readonly void SetIconFireBig()
-            {
-                IconRawImage.texture = UIRepairComponentView.instance.fire;
-            }
-            public readonly void SetIconColorNormal()
-            {
-                IconRawImage.color = _colorWhite;
-            }
-
-            public readonly void SetIconColorNormalFaded()
-            {
-                IconRawImage.color = _colorWhiteTransparent;
-            }
-
-            public readonly void SetIconColorDamaged()
-            {
-                IconRawImage.color = _colorRedTransparent;
-            }
-
-            public readonly void SetIconColorDestroyed()
-            {
-                IconRawImage.color = _colorRed;
-            }
-
-            public override readonly string ToString()
-            {
-                return $"{Name}";
-            }
-        }
+        private static int _indexBalloon = -1;
+        private static int _indexArmor = -1;
+        private static List<int> _indexGuns = [];
+        private static List<int> _indexEngines = [];
 
         [HarmonyPatch(typeof(UIManager.UINewMainMenuState), nameof(UIManager.UINewMainMenuState.Enter))]
         [HarmonyPostfix]
@@ -80,6 +32,88 @@ namespace BuffKit.RepairCluster
             if (!_firstMainMenuState) return;
             _firstMainMenuState = false;
             CreateUi();
+        }
+
+        private void LateUpdate()
+        {
+            if (NetworkedPlayer.Local?.CurrentShip == null) return;
+
+            UpdateIndicators();
+        }
+
+        private static void UpdateIndicators()
+        {
+            var repairables = NetworkedPlayer.Local.CurrentShip.Repairables;
+            _indexBalloon = -1;
+            _indexArmor = -1;
+            _indexGuns = [];
+            _indexEngines = [];
+
+            for (var index = 0; index < repairables.Count; index++)
+            {
+                var repairable = repairables[index];
+                if (repairable as Turret != null) { _indexGuns.Add(index); continue; }
+                if (repairable as Engine != null) { _indexEngines.Add(index); continue; }
+                if (repairable as Balloon != null) { _indexBalloon = index; continue; }
+                if (repairable as Hull != null) { _indexArmor = index; continue; }
+            }
+
+            var gunsDone = 0;
+            var enginesDone = 0;
+
+            for (var index = 0; index < _indicators.Count; index++)
+            {
+                var indicator = _indicators[index];
+                var name = indicator.Name.ToLower();
+                if (name.Contains("gun"))
+                {
+                    if (_indexGuns.Count > gunsDone)
+                    {
+                        indicator.Active = true;
+                        var repairableIndex = _indexGuns[gunsDone];
+                        indicator.SetHealth(repairables[repairableIndex].NormalizedHealth);
+                        gunsDone++;
+                        continue;
+                    }
+                    indicator.Active = false;
+                    continue;
+                }
+                if (name.Contains("engine"))
+                {
+                    if (_indexEngines.Count > enginesDone)
+                    {
+                        indicator.Active = true;
+                        var repairableIndex = _indexEngines[enginesDone];
+                        indicator.SetHealth(repairables[repairableIndex].NormalizedHealth);
+                        enginesDone++;
+                        continue;
+                    }
+                    indicator.Active = false;
+                    continue;
+                }
+                if (name.Contains("balloon"))
+                {
+                    if (_indexBalloon > -1)
+                    {
+                        indicator.Active = true;
+                        indicator.SetHealth(repairables[_indexBalloon].NormalizedHealth);
+                        continue;
+                    }
+                    indicator.Active = false;
+                    continue;
+                }
+                if (name.Contains("armor"))
+                {
+                    if (_indexArmor > -1)
+                    {
+                        indicator.Active = true;
+                        indicator.SetHealth(repairables[_indexArmor].NormalizedHealth);
+                        continue;
+                    }
+                    indicator.Active = false;
+                    continue;
+                }
+            }
         }
 
         private static GameObject CreateUi()
@@ -106,6 +140,7 @@ namespace BuffKit.RepairCluster
                 CreateIndicator($"Engine {index + 1}", UIRepairComponentView.instance.enginesBig, mainObject.transform);
             }
 
+            mainObject.AddComponent<RepairCluster>();
             return mainObject;
         }
 
@@ -139,29 +174,85 @@ namespace BuffKit.RepairCluster
             vlg.childForceExpandWidth = false;
             vlg.padding = _progressBarPadding;
 
-            CreateProgressBar("Health", 4, progressBarsObject.transform, out LayoutElement progressBarLe);
+            CreateProgressBar("Health", 4, progressBarsObject.transform, out LayoutElement healthBarLe, out RawImage healthBarRawImage);
 
             var indicator = new Indicator
             {
                 Name = name,
+                IndicatorObject = indicatorObject,
                 ComponentTexture = componentTexture,
                 IconRawImage = rawImage,
-                ProgressBarLayoutElement = progressBarLe,
+                HealthBarLayoutElement = healthBarLe,
+                HealthBarRawImage = healthBarRawImage,
             };
             _indicators.Add(indicator);
             return indicatorObject;
         }
 
-        private static GameObject CreateProgressBar(string name, int height, Transform parentTransform, out LayoutElement layoutElement)
+        private static GameObject CreateProgressBar(string name, int height, Transform parentTransform, out LayoutElement layoutElement, out RawImage rawImage)
         {
             var progressBarObject = new GameObject($"Progress Bar {name}");
             progressBarObject.transform.SetParent(parentTransform, false);
             layoutElement = progressBarObject.AddComponent<LayoutElement>();
             layoutElement.preferredHeight = height;
             layoutElement.preferredWidth = _gridWidth - _progressBarPadding.horizontal;
-            var rawImage = progressBarObject.AddComponent<RawImage>();
+            rawImage = progressBarObject.AddComponent<RawImage>();
             rawImage.color = _colorWhite;
             return progressBarObject;
+        }
+
+        private class Indicator
+        {
+            public string Name;
+            public GameObject IndicatorObject;
+            public Texture2D ComponentTexture;
+            public RawImage IconRawImage;
+            public LayoutElement HealthBarLayoutElement;
+            public RawImage HealthBarRawImage;
+
+            public bool Active { get => IndicatorObject.activeSelf; set { if (IndicatorObject.activeSelf != value) IndicatorObject.SetActive(value); } }
+
+            public void SetHealth(float percentage)
+            {
+                // Set color.
+                if (percentage == 0f)
+                {
+                    IconRawImage.color = _colorRedDestroyed;
+                }
+                else if (percentage <= .5f)
+                {
+                    IconRawImage.color = _colorOrangeDamaged;
+                    HealthBarRawImage.color = _colorOrangeDamaged;
+                }
+                else
+                {
+                    IconRawImage.color = _colorWhiteTransparent;
+                    HealthBarRawImage.color = _colorWhiteTransparent;
+                }
+                // Hide bar if health is full.
+                if (percentage == 1f) percentage = 0;
+                HealthBarLayoutElement.preferredWidth = percentage * (_gridWidth - _progressBarPadding.horizontal);
+            }
+
+            public void SetIconNormal()
+            {
+                IconRawImage.texture = ComponentTexture;
+            }
+
+            public void SetIconFire()
+            {
+                IconRawImage.texture = UIRepairComponentView.instance.fireNormal;
+            }
+
+            public void SetIconFireBig()
+            {
+                IconRawImage.texture = UIRepairComponentView.instance.fire;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name}";
+            }
         }
     }
 }
