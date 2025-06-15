8e40e3b9d45178005f3cbb98a0f73d29b621430b
Pit <drpitlazarus@gmail.com>
2025-06-15 19:29:53 -0400

    BuffKit: Add BuffKit GameObject

--- BuffKit/BuffKit.cs
+++ BuffKit/BuffKit.cs
@@ -1,16 +1,19 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using UnityEngine;
 
 namespace BuffKit
 {
     [BepInPlugin("me.trgk.buffkit", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class BuffKit : BaseUnityPlugin
     {
+        public static GameObject GameObject { get; private set; }
+
         private void Awake()
         {
-            var harmony = new Harmony("BuffKit");
-
+            var harmony = new Harmony(PluginInfo.PLUGIN_NAME);
             harmony.PatchAll();
+            GameObject = new GameObject(PluginInfo.PLUGIN_NAME);
             MuseLog.Info("Buff applied!");
         }
     }
